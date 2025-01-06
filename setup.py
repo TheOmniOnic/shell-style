83b3c5ac76f271e00b5fb35e6555dbe0f91e1878
TheOmniOnic <1950narayandebnath@gmail.com>
2025-01-06 22:17:53 +0530

    Incremented version and fixed license

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="shell-style",  
-    version="0.0.1",  
+    version="0.0.2",
     description="A terminal text formatting package with 24-bit color support",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
@@ -15,8 +15,8 @@ setup(
     python_requires=">=3.6",  
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPL License",
-        "Operating System :: Cross Platform",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
         "Environment :: Console",
         "Topic :: Terminals",
     ],
