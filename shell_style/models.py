4375e6ed84b2e8095d62eb469367293fc0a2355c
TheOmniOnic <1950narayandebnath@gmail.com>
2025-02-01 23:09:37 +0530

    Fixed the darn enums

--- shell_style/models.py
+++ shell_style/models.py
@@ -3,19 +3,18 @@ Classes for Shell-Style package.
 This module defines _BaseObject, Theme, Console, ProgressBar and Table.
 """
 
-from time import sleep
-from os import get_terminal_size
-from abc import ABC
-from typing import Any
-from .ssml import interpret
-from .enums import (Styles, BackgroundColors, ForegroundColors, Other)
+from time import sleep as _sleep
+from os import get_terminal_size as _get_terminal_size
+from abc import ABC as _ABC
+from typing import Any as _Any
+from .ssml import interpret as _interpret
+from .enums import (Styles as _Styles, BackgroundColors as _BackgroundColors, ForegroundColors as _ForegroundColors,  Other as _Other)
 
-# Set __all__
 __all__ = ["Theme", "Console", "Table", "ProgressBar", "DEFAULT_THEME"]
 
-class _BaseObject(ABC):
+class _BaseObject(_ABC):
     """
-    Abstract Base Class for all other classes defined here.
+    Abstract Base Class for all _Other classes defined here.
     Defines the help method.
     
     Args: None
@@ -23,7 +22,7 @@ class _BaseObject(ABC):
     
     @classmethod
     def help(cls) -> str:
-        return interpret(f"<@heading>{cls.__name__}<@stop>\n{cls.__doc__}")
+        return _interpret(f"<@heading>{cls.__name__}<@stop>\n{cls.__doc__}")
 
 class Theme(_BaseObject):
     """
@@ -34,35 +33,35 @@ class Theme(_BaseObject):
     """
     
     def __init__(self, **styles) -> None:
-        self.__styles = styles
+        self._styles = styles
         
     @property
     def styles(self) -> dict[str, str]:
-        return self.__styles
+        return self._styles
     
     @styles.setter
     def styles(self, styles: dict[str, str]) -> None:
-        self.__styles.update(styles)
+        self._styles.update(styles)
         
     def get_style(self, target: str) -> str:
-        return str(self.__styles.get(target))
+        return str(self._styles.get(target))
     
-    def __getitem__(self, target: str) -> Any:
-        return self.__styles.get(target)
+    def __getitem__(self, target: str) -> _Any:
+        return self._styles.get(target)
     
     def __setitem__(self, style: str, value: str) -> None:
-        self.__styles[style] = value
+        self._styles[style] = value
 
 DEFAULT_THEME = Theme(
-    info=ForegroundColors.CYAN, warning=ForegroundColors.YELLOW, error=ForegroundColors.RED, 
-    success=ForegroundColors.GREEN, bold=Styles.BOLD, dim=Styles.DIM, italic=Styles.ITALIC, 
-    underline=Styles.UNDERLINE, blink=Styles.BLINK, inverse=Styles.INVERSE, hidden=Styles.HIDDEN, 
-    strikethrough=Styles.STRIKETHROUGH, fg_black=ForegroundColors.BLACK, fg_white=ForegroundColors.WHITE, 
-    fg_green=ForegroundColors.GREEN, fg_yellow=ForegroundColors.YELLOW, fg_blue=ForegroundColors.BLUE, 
-    fg_magenta=ForegroundColors.MAGENTA, fg_cyan=ForegroundColors.CYAN, bg_black=BackgroundColors.BLACK,
-    bg_red=BackgroundColors.RED, bg_blue=BackgroundColors.BLUE, stop=Other.STOP,
-    bg_green=BackgroundColors.GREEN, bg_magenta=BackgroundColors.MAGENTA, bg_cyan=BackgroundColors.CYAN,
-    bg_white=BackgroundColors.WHITE, default=Styles.DEFAULT, heading=Styles.BOLD + Styles.UNDERLINE
+    info=_ForegroundColors.CYAN, warning=_ForegroundColors.YELLOW, error=_ForegroundColors.RED, 
+    success=_ForegroundColors.GREEN, bold=_Styles.BOLD, dim=_Styles.DIM, italic=_Styles.ITALIC, 
+    underline=_Styles.UNDERLINE, blink=_Styles.BLINK, inverse=_Styles.INVERSE, hidden=_Styles.HIDDEN, 
+    strikethrough=_Styles.STRIKETHROUGH, fg_black=_ForegroundColors.BLACK, fg_white=_ForegroundColors.WHITE, 
+    fg_green=_ForegroundColors.GREEN, fg_yellow=_ForegroundColors.YELLOW, fg_blue=_ForegroundColors.BLUE, 
+    fg_magenta=_ForegroundColors.MAGENTA, fg_cyan=_ForegroundColors.CYAN, bg_black=_BackgroundColors.BLACK,
+    bg_red=_BackgroundColors.RED, bg_blue=_BackgroundColors.BLUE, stop=_Other.STOP,
+    bg_green=_BackgroundColors.GREEN, bg_magenta=_BackgroundColors.MAGENTA, bg_cyan=_BackgroundColors.CYAN,
+    bg_white=_BackgroundColors.WHITE, default=_Styles.DEFAULT, heading=_Styles.BOLD + _Styles.UNDERLINE
     )
 
 class Console(_BaseObject):
@@ -82,7 +81,7 @@ class Console(_BaseObject):
             self.print_title()
         
     def print_title(self) -> None:
-        self.write(f"<@heading>{self.__title}<@stop>", alignment=Other.CENTER.value)
+        self.write(f"<@heading>{self.__title}<@stop>", alignment=_Other.CENTER)
         
     @property
     def theme(self) -> Theme:
@@ -110,19 +109,19 @@ class Console(_BaseObject):
         Returns: None
         """
         
-        print(Other.CLEAR, end="")
+        print(_Other.CLEAR, end="")
         if print_title:
             self.print_title()
         
-    def log(self, *objects, end: str = f"{Other.STOP}\n", sep: str = " ", 
-            style: str = "default", alignment: str = Other.LEFT.value) -> None:
+    def log(self, *objects, end: str = f"{_Other.STOP}\n", sep: str = " ", 
+            style: str = "default", alignment: str = _Other.LEFT) -> None:
         """
         Customized log method.
         
         Args: 
-            *objects: Any, 
-            end: str = f"{Other.STOP}\\n",
-            alignment: str = Other.LEFT,
+            *objects: _Any, 
+            end: str = f"{_Other.STOP}\\n",
+            alignment: str = _Other.LEFT,
             sep: str = " ", 
             style: str = "default"
             
@@ -130,18 +129,18 @@ class Console(_BaseObject):
         """
         
         print(
-        interpret(self.__align_text(self.__theme.get_style(style) + "".join(map(str, objects)), alignment)),
+        _interpret(self.__align_text(self.__theme.get_style(style) + "".join(map(str, objects)), alignment)),
         end=end, sep=sep)
             
-    def write(self, *objects: Any, alignment: str = Other.LEFT.value, end: str = f"{Other.STOP}\n", 
+    def write(self, *objects: _Any, alignment: str = _Other.LEFT, end: str = f"{_Other.STOP}\n", 
               sep: str = " ", style: str = "default") -> None:
         """
         Customized print method.
         
         Args:
-            *objects: Any,
+            *objects: _Any,
             alignment: str, 
-            end: str = Other.STOP, 
+            end: str = _Other.STOP, 
             sep: str = "", 
             style: str
         
@@ -149,38 +148,38 @@ class Console(_BaseObject):
         """
         
         print(
-        interpret(self.__align_text(self.__theme.get_style(style) + "".join(map(str, objects)), alignment)),
+        _interpret(self.__align_text(self.__theme.get_style(style) + "".join(map(str, objects)), alignment)),
         end=end, sep=sep
             )
         
-    def prompt(self, *objects, end: str = Other.STOP.value, style: str = "default") -> str:
+    def prompt(self, *objects, end: str = _Other.STOP, style: str = "default") -> str:
         """
         Customized input method.
         
         Args:
-            *objects: Any,
-            end: str = Other.STOP, 
+            *objects: _Any,
+            end: str = _Other.STOP, 
             style: str
         
         Returns: str
         """
         
         try:
-            text = input(interpret(self.__theme.get_style(style) + "".join(map(str, objects))) + end)
+            text = input(_interpret(self.__theme.get_style(style) + "".join(map(str, objects))) + end)
         
-        except EOFError:
+        except (EOFError, KeyboardInterrupt):
             return ""
         
         else:
             return text
     
     @staticmethod
-    def __align_text(text: Any, alignment: str) -> str:
+    def __align_text(text: _Any, alignment: str) -> str:
         """
         Private static method for text alignment.
         
         Args:
-            text: Any, 
+            text: _Any, 
             alignment: str
         
         Returns: str 
@@ -188,16 +187,16 @@ class Console(_BaseObject):
         Raises: ValueError (if alignment is not valid)
         """
         
-        width = get_terminal_size().columns
+        width = _get_terminal_size().columns
         
-        if alignment == Other.CENTER:
+        if alignment == _Other.CENTER:
             padding = (width - len(text)) // 2
             return " " * padding + text + " " * (width - len(text) - padding)
         
-        elif alignment == Other.RIGHT:
+        elif alignment == _Other.RIGHT:
             return (" " * (width - len(text)) + text).rstrip(" ")
         
-        elif alignment == Other.LEFT:
+        elif alignment == _Other.LEFT:
             return (text + " " * (width - len(text))).lstrip(" ")
         
         else:
@@ -218,7 +217,7 @@ class ProgressBar(_BaseObject):
                  symbol: str = "-", delay: float = 1) -> None:
         self.__values = values
         self.__theme = theme
-        self.__symbol = interpret(symbol)
+        self.__symbol = _interpret(symbol)
         self.__delay = delay
         
     @property
@@ -243,7 +242,7 @@ class ProgressBar(_BaseObject):
     
     @symbol.setter
     def symbol(self, new: str) -> None:
-        self.__symbol = interpret(new)
+        self.__symbol = _interpret(new)
         
     @property
     def delay(self) -> float:
@@ -264,8 +263,8 @@ class ProgressBar(_BaseObject):
         """
         
         for _ in range(self.__values):
-            print(self.__theme.get_style(style) + self.__symbol, end=Other.STOP.value, flush=True)
-            sleep(self.__delay)
+            print(self.__theme.get_style(style) + self.__symbol, end=_Other.STOP, flush=True)
+            _sleep(self.__delay)
             
 class Table(_BaseObject):
     """
@@ -284,12 +283,12 @@ class Table(_BaseObject):
         self.__rows = 0
         self.__table = []
         
-    def add_row(self, *objects: Any, style: str = "default") -> None:
+    def add_row(self, *objects: _Any, style: str = "default") -> None:
         """
         Add a row to self.__table.
         
         Args:
-            *objects: Any
+            *objects: _Any
             
         Returns: None
         """
@@ -297,7 +296,7 @@ class Table(_BaseObject):
         interpreted_objects = []
         
         for obj in objects:
-            interpreted_objects.append(interpret(self.__theme.get_style(style) + str(obj) + Other.STOP.value))
+            interpreted_objects.append(_interpret(self.__theme.get_style(style) + str(obj) + _Other.STOP))
             
         while len(interpreted_objects) < self.__columns:
             interpreted_objects.append(None)
@@ -336,24 +335,24 @@ class Table(_BaseObject):
             
         self.__columns -= 1
         
-    def add_column(self, placeholder: Any = "", style: str = "default") -> None:
+    def add_column(self, placeholder: _Any = "", style: str = "default") -> None:
         """
         Add a column in self.__table.
         
         Args:
-            placeholder: Any = ""
+            placeholder: _Any = ""
             
         Returns: None
         """
         
-        placeholder = interpret(self.__theme.get_style(style) + str(placeholder) + Other.STOP.value)
+        placeholder = _interpret(self.__theme.get_style(style) + str(placeholder) + _Other.STOP)
         
         for row in self.__table:
             row.append(placeholder)
         
         self.__columns += 1
             
-    def get_column(self, row_index: int, column_index: int) -> Any:
+    def get_column(self, row_index: int, column_index: int) -> _Any:
         """
         Get the information in a column in self.__table.
         
@@ -361,24 +360,24 @@ class Table(_BaseObject):
             row_index: int,
             column_index: int
             
-        Returns: Any
+        Returns: _Any
         """
         
         return self.__table[row_index][column_index]
     
-    def set_column(self, info: Any, row_index: int, column_index: int, style: str = "default") -> None:
+    def set_column(self, info: _Any, row_index: int, column_index: int, style: str = "default") -> None:
         """
         Set the information in a column in self.__table.
         
         Args:
-            info: Any,
+            info: _Any,
             row_index: int,
             column_index: int
             
         Returns: None
         """
         
-        self.__table[row_index][column_index] = interpret(self.__theme.get_style(style) + str(info) + Other.STOP.value)
+        self.__table[row_index][column_index] = _interpret(self.__theme.get_style(style) + str(info) + _Other.STOP)
         
     def get_row(self, index: int) -> list:
         """
@@ -457,7 +456,7 @@ class Table(_BaseObject):
         return self.__columns
     
     @property
-    def table(self) -> list[list[Any]]:
+    def table(self) -> list[list[_Any]]:
         return self.__table
     
     @property
--- shell_style/ssml.py
+++ shell_style/ssml.py
@@ -1,13 +1,12 @@
-from .enums import (Styles, ForegroundColors, BackgroundColors, Other)
-from re import sub
+from .enums import (Styles as _Styles, ForegroundColors as _ForegroundColors, BackgroundColors as _BackgroundColors, Other as _Other)
 
 def interpret(text: str, mode: int = 0) -> str:
     """
-    Interprets text into SSML (Shell-Style Markup Language) or ANSI escape sequences depending on the mode
+    Interpret SSML to ANSI, ANSI to SSML, HTML to SSML or SSML to HTML dependng on the mode
     
     Args:
         text: str,
-        mode: int = 0 (must be 0 or 1)
+        mode: int = 0 (must be between 0 and 3)
         
     Returns: str 
     """
@@ -17,12 +16,19 @@ def interpret(text: str, mode: int = 0) -> str:
     
     elif mode == 1:
         return ansi_to_ssml(text)
+    
+    elif mode == 2:
+        return ssml_to_html(text)
+    
+    elif mode == 3:
+        return html_to_ssml(text)
 
-    raise ValueError(f"Invalid 'mode' argument: {mode}. Must be 1 or 0")
+    else:
+        raise ValueError(f"Invalid 'mode' argument: {mode}. Must be between 0 and 3")
 
 def ssml_to_ansi(text: str) -> str:
     """
-    Interprets SSML (Shell-Style Markup Language) text into ANSI escape sequences for terminal styling.
+    Interprets SSML (Shell-Style Markup Language) text into ANSI escape sequences
     
     Args:
         text: str 
@@ -30,46 +36,46 @@ def ssml_to_ansi(text: str) -> str:
     Returns: str 
     """
     
-    ssml_to_ansi = {
-        "<@bold>": Styles.BOLD,
-        "<@dim>": Styles.DIM,
-        "<@italic>": Styles.ITALIC,
-        "<@underline>": Styles.UNDERLINE,
-        "<@blink>": Styles.BLINK,
-        "<@inverse>": Styles.INVERSE,
-        "<@hidden>": Styles.HIDDEN,
-        "<@strikethrough>": Styles.STRIKETHROUGH,
-        "<@fg_black>": ForegroundColors.BLACK,
-        "<@fg_red>": ForegroundColors.RED,
-        "<@fg_green>": ForegroundColors.GREEN,
-        "<@fg_yellow>": ForegroundColors.YELLOW,
-        "<@fg_blue>": ForegroundColors.BLUE,
-        "<@fg_magenta>": ForegroundColors.MAGENTA,
-        "<@fg_cyan>": ForegroundColors.CYAN,
-        "<@fg_white>": ForegroundColors.WHITE,
-        "<@bg_black>": BackgroundColors.BLACK,
-        "<@bg_red>": BackgroundColors.RED,
-        "<@bg_green>": BackgroundColors.GREEN,
-        "<@bg_blue>": BackgroundColors.BLUE,
-        "<@bg_magenta>": BackgroundColors.MAGENTA,
-        "<@bg_cyan>": BackgroundColors.CYAN,
-        "<@bg_white>": BackgroundColors.WHITE,
-        "<@stop>": Other.STOP,
-        "<@info>": ForegroundColors.CYAN,
-        "<@success>": ForegroundColors.GREEN,
-        "<@warning>": ForegroundColors.YELLOW,
-        "<@error>": ForegroundColors.RED,
-        "<@heading>": Styles.BOLD.value + Styles.UNDERLINE.value
+    ssml_to_ansi_map = {
+        "<@bold>": _Styles.BOLD.value,
+        "<@dim>": _Styles.DIM.value,
+        "<@italic>": _Styles.ITALIC.value,
+        "<@underline>": _Styles.UNDERLINE.value,
+        "<@blink>": _Styles.BLINK.value,
+        "<@inverse>": _Styles.INVERSE.value,
+        "<@hidden>": _Styles.HIDDEN.value,
+        "<@strikethrough>": _Styles.STRIKETHROUGH.value,
+        "<@fg_black>": _ForegroundColors.BLACK.value,
+        "<@fg_red>": _ForegroundColors.RED.value,
+        "<@fg_green>": _ForegroundColors.GREEN.value,
+        "<@fg_yellow>": _ForegroundColors.YELLOW.value,
+        "<@fg_blue>": _ForegroundColors.BLUE.value,
+        "<@fg_magenta>": _ForegroundColors.MAGENTA.value,
+        "<@fg_cyan>": _ForegroundColors.CYAN.value,
+        "<@fg_white>": _ForegroundColors.WHITE.value,
+        "<@bg_black>": _BackgroundColors.BLACK.value,
+        "<@bg_red>": _BackgroundColors.RED.value,
+        "<@bg_green>": _BackgroundColors.GREEN.value,
+        "<@bg_blue>": _BackgroundColors.BLUE.value,
+        "<@bg_magenta>": _BackgroundColors.MAGENTA.value,
+        "<@bg_cyan>": _BackgroundColors.CYAN.value,
+        "<@bg_white>": _BackgroundColors.WHITE.value,
+        "<@stop>": _Other.STOP.value,
+        "<@info>": _ForegroundColors.CYAN.value,
+        "<@success>": _ForegroundColors.GREEN.value,
+        "<@warning>": _ForegroundColors.YELLOW.value,
+        "<@error>": _ForegroundColors.RED.value,
+        "<@heading>": (_Styles.BOLD.value + _Styles.UNDERLINE.value)
     }
 
-    for ssml, ansi in ssml_to_ansi.items():
+    for ssml, ansi in ssml_to_ansi_map.items():
         text = text.replace(ssml, ansi)
 
     return text
 
 def ansi_to_ssml(text: str) -> str:
     """
-    Interprets ANSI escape sequences into SSML (Shell-Style Markup Language) text for terminal styling.
+    Interprets ANSI escape sequences into SSML (Shell-Style Markup Language) text
     
     Args:
         text: str 
@@ -78,24 +84,132 @@ def ansi_to_ssml(text: str) -> str:
     """
     
     ansi_to_ssml_map = {
-        r"\033\[1m": "<@bold>",                  
-        r"\033\[2m": "<@dim>",                 
-        r"\033\[3m": "<@italic>",              
-        r"\033\[4m": "<@underline>",            
-        r"\033\[5m": "<@blink>",                
-        r"\033\[7m": "<@inverse>",              
-        r"\033\[8m": "<@hidden>",               
-        r"\033\[9m": "<@strikethrough>",         
-        r"\033\[38;2;(\d+);(\d+);(\d+)m": r"<@fg_rgb(\1,\2,\3)>",  
-        r"\033\[48;2;(\d+);(\d+);(\d+)m": r"<@bg_rgb(\1,\2,\3)>", 
-        r"\033\[38;5;(\d+)m": r"<@fg_color(\1)>",  
-        r"\033\[48;5;(\d+)m": r"<@bg_color(\1)>", 
-        r"\033\[38;2;([0-9a-fA-F]{6})m": r"<@fg_hex(\1)>",  
-        r"\033\[48;2;([0-9a-fA-F]{6})m": r"<@bg_hex(\1)>", 
-        r"\033\[0m": "<@stop>",                  
+        _Styles.BOLD.value: "<@bold>",
+        _Styles.DIM.value: "<@dim>",
+        _Styles.ITALIC.value: "<@italic>",
+        _Styles.UNDERLINE.value: "<@underline>",
+        _Styles.BLINK.value: "<@blink>",
+        _Styles.INVERSE.value: "<@inverse>",
+        _Styles.HIDDEN.value: "<@hidden>",
+        _Styles.STRIKETHROUGH.value: "<@strikethrough>",
+        _ForegroundColors.BLACK.value: "<@fg_black>",
+        _ForegroundColors.RED.value: "<@fg_red>",
+        _ForegroundColors.GREEN.value: "<@fg_green>",
+        _ForegroundColors.YELLOW.value: "<@fg_yellow>",
+        _ForegroundColors.BLUE.value: "<@fg_blue>",
+        _ForegroundColors.MAGENTA.value: "<@fg_magenta>",
+        _ForegroundColors.CYAN.value: "<@fg_cyan>",
+        _ForegroundColors.WHITE.value: "<@fg_white>",
+        _BackgroundColors.BLACK.value: "<@bg_black>",
+        _BackgroundColors.RED.value: "<@bg_red>",
+        _BackgroundColors.GREEN.value: "<@bg_green>",
+        _BackgroundColors.BLUE.value: "<@bg_blue>",
+        _BackgroundColors.MAGENTA.value: "<@bg_magenta>",
+        _BackgroundColors.CYAN.value: "<@bg_cyan>",
+        _BackgroundColors.WHITE.value: "<@bg_white>",
+        _Other.STOP.value: "<@stop>",
+        _ForegroundColors.CYAN.value: "<@info>",
+        _ForegroundColors.GREEN.value: "<@success>",
+        _ForegroundColors.YELLOW.value: "<@warning>",
+        _ForegroundColors.RED.value: "<@error>",
+        (_Styles.BOLD.value + _Styles.UNDERLINE.value): "<@heading>",               
     }
 
     for ansi, ssml in ansi_to_ssml_map.items():
-        text = sub(ansi, ssml, text)
+        text = text.replace(ansi, ssml)
+
+    return text
+
+def ssml_to_html(text: str) -> str:
+    """
+    Interprets SSML (Shell Style Markup Language) text into HTML text
+
+    Args:
+        text: str
+
+    Returns: str
+    """
+
+    ssml_to_html_map = {
+        "<@bold>": "<span style='font-weight:bold'>",
+        "<@dim>": "<span style='opacity:0.6'>",
+        "<@italic>": "<span style='font-style:italic'>",
+        "<@underline>": "<span style='text-decoration:underline'>",
+        "<@blink>": "<span style='animation:blink 1s step-end infinite'>",
+        "<@inverse>": "<span style='color:white;background-color:black'>",
+        "<@hidden>": "<span style='visibility:hidden'>",
+        "<@strikethrough>": "<span style='text-decoration:line-through'>",
+        "<@fg_black>": "<span style='color:black'>",
+        "<@fg_red>": "<span style='color:red'>",
+        "<@fg_green>": "<span style='color:green'>",
+        "<@fg_yellow>": "<span style='color:yellow'>",
+        "<@fg_blue>": "<span style='color:blue'>",
+        "<@fg_magenta>": "<span style='color:magenta'>",
+        "<@fg_cyan>": "<span style='color:cyan'>",
+        "<@fg_white>": "<span style='color:white'>",
+        "<@bg_black>": "<span style='background-color:black'>",
+        "<@bg_red>": "<span style='background-color:red'>",
+        "<@bg_green>": "<span style='background-color:green'>",
+        "<@bg_blue>": "<span style='background-color:blue'>",
+        "<@bg_magenta>": "<span style='background-color:magenta'>",
+        "<@bg_cyan>": "<span style='background-color:cyan'>",
+        "<@bg_white>": "<span style='background-color:white'>",
+        "<@stop>": "</span>",
+        "<@info>": "<span style='color:cyan'>",
+        "<@success>": "<span style='color:green'>",
+        "<@warning>": "<span style='color:yellow'>",
+        "<@error>": "<span style='color:red'>",
+        "<@heading>": "<span style='font-weight:bold;text-decoration:underline'>"
+    }
+
+    for ssml, html in ssml_to_html_map.items:
+        text = text.replace(ssml, html)
+
+    return text
+
+def html_to_ssml(text: str) -> str:
+    """
+    Interprets HTML text into SSML (Shell Style Markup Language) text
+
+    Args:
+        text: str
+
+    Returns: str
+    """
+
+    html_to_ssml_map = {
+        "<span style='font-weight:bold'>": "<@bold>",
+        "<span style='opacity:0.6'>": "<@dim>",
+        "<span style='font-style:italic'>": "<@italic>",
+        "<span style='text-decoration:underline'>": "<@underline>",
+        "<span style='animation:blink 1s step-end infinite'>": "<@blink>",
+        "<span style='color:white;background-color:black'>": "<@inverse>",
+        "<span style='visibility:hidden'>": "<@hidden>",
+        "<span style='text-decoration:line-through'>": "<@strikethrough>",
+        "<span style='color:black'>": "<@fg_black>",
+        "<span style='color:red'>": "<@fg_red>",
+        "<span style='color:green'>": "<@fg_green>",
+        "<span style='color:yellow'>": "<@fg_yellow>",
+        "<span style='color:blue'>": "<@fg_blue>",
+        "<span style='color:magenta'>": "<@fg_magenta>",
+        "<span style='color:cyan'>": "<@fg_cyan>",
+        "<span style='color:white'>": "<@fg_white>",
+        "<span style='background-color:black'>": "<@bg_black>",
+        "<span style='background-color:red'>": "<@bg_red>",
+        "<span style='background-color:green'>": "<@bg_green>",
+        "<span style='background-color:blue'>": "<@bg_blue>",
+        "<span style='background-color:magenta'>": "<@bg_magenta>",
+        "<span style='background-color:cyan'>": "<@bg_cyan>",
+        "<span style='background-color:white'>": "<@bg_white>",
+        "</span>": "<@stop>",
+        "<span style='color:cyan'>": "<@info>",
+        "<span style='color:green'>": "<@success>",
+        "<span style='color:yellow'>": "<@warning>",
+        "<span style='color:red'>": "<@error>",
+        "<span style='font-weight:bold;text-decoration:underline'>": "<@heading>"
+    }
+
+    for html, ssml in html_to_ssml_map.items:
+        text = text.replace(html, ssml)
 
     return text
